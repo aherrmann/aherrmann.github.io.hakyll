55260f51e172b4c035b6512e6ffecf031ddafb79
Andreas Herrmann <andreash87@gmx.ch>
2016-01-26 20:51:21 +0100

    Title text outline

--- my-site/css/default.hs
+++ my-site/css/default.hs
@@ -1,5 +1,6 @@
 {-# LANGUAGE OverloadedStrings #-}
 import           Clay
+import           Clay.Stylesheet (key)
 import           Data.Monoid
 import qualified Data.Text.Lazy.IO as T
 import           Prelude hiding ((**),rem)
@@ -10,41 +11,6 @@ main = T.putStr $ renderWith pretty [] mainCss
 
 
 --------------------------------------------------------------------------------
-whiteColor :: Color
-whiteColor = "#F6F2F1"
-
-lightBlueColor :: Color
-lightBlueColor = "#8A96A6"
-
-blueColor :: Color
-blueColor = "#44495D"
-
-darkBlueColor :: Color
-darkBlueColor = "#242335"
-
-
---------------------------------------------------------------------------------
-bodyBgColor :: Color
-bodyBgColor = whiteColor
-
-titleBgColor :: Color
-titleBgColor = darkBlueColor
-
-titleH1Color :: Color
-titleH1Color = whiteColor
-
-titleH2Color :: Color
-titleH2Color = lightBlueColor
-
-menuItemColor :: Color
-menuItemColor = lightBlueColor
-
-menuItemBgColor :: Color
-menuItemBgColor = blueColor
-
-footerColor :: Color
-footerColor = lightBlueColor
-
 headings :: Selector
 headings = h1 <> h2 <> h3 <> h4 <> h5 <> h6
 
@@ -53,8 +19,113 @@ headings_ = headings <> headings ** star
 
 
 --------------------------------------------------------------------------------
-mainCss :: Css
-mainCss = do
+textStroke :: Color -> Css
+textStroke c = do
+    key "-webkit-text-stroke" $ value (px 3) <> " " <> value c
+    key "text-shadow" $ [ shadow x y | (x, y) <- xys ]
+  where
+    shadow x y = (value $ px x) <> " "
+              <> (value $ px y) <> " "
+              <> (value $ px 0) <> " "
+              <> (value $ c)
+    xys = [ ((-1),    1), (0,    2), (1,    1)
+          , ((-2),    0),            (2,    0)
+          , ((-1), (-1)), (0, (-2)), (1, (-1)) ]
+
+fontSmooth :: Css
+fontSmooth = do
+    "-webkit-font-smoothing" -: "subpixel-antialiased" -- "antialiased"
+    "-moz-osx-font-smoothing" -: "grayscale"
+
+
+--------------------------------------------------------------------------------
+color1 :: Color
+color1 = "#354458"
+
+color2 :: Color
+color2 = "#3A9AD9"
+
+color3 :: Color
+color3 = "#29ABA4"
+
+color4 :: Color
+color4 = "#E9E0D6"
+
+color5 :: Color
+color5 = "#EB7260"
+
+
+--------------------------------------------------------------------------------
+pageBackgroundColor :: Color
+pageBackgroundColor = color4
+
+sectionTitleColor :: Color
+sectionTitleColor = pageBackgroundColor -. 50
+
+sectionBackgroundColor :: Color
+sectionBackgroundColor = pageBackgroundColor -. 10
+
+sectionBackground :: Css
+sectionBackground = do
+    backgroundColor sectionBackgroundColor
+    background $
+        vGradient (sectionBackgroundColor -. 10) sectionBackgroundColor
+
+menuBackgroundColor :: Color
+menuBackgroundColor = color4 -. 20
+
+menuBackground :: Css
+menuBackground = do
+    backgroundColor menuBackgroundColor
+    background $
+        vGradient (menuBackgroundColor -. 10) menuBackgroundColor
+
+menuItemColor :: Color
+menuItemColor = color1 -. 30
+
+menuHoverBackgroundColor :: Color
+menuHoverBackgroundColor = color2
+
+menuHoverBackground :: Css
+menuHoverBackground = do
+    backgroundColor menuHoverBackgroundColor
+    background $
+        vGradient (menuHoverBackgroundColor -. 10) menuHoverBackgroundColor
+
+menuItemHoverColor :: Color
+menuItemHoverColor = color1 -. 20
+
+headerBackgroundColor :: Color
+headerBackgroundColor = color1
+
+headerBackground :: Css
+headerBackground = do
+    backgroundColor headerBackgroundColor
+    background $
+        vGradient (headerBackgroundColor -. 10) headerBackgroundColor
+
+headerTitleColor :: Color
+headerTitleColor = color4
+
+headerTitleBorderColor :: Color
+headerTitleBorderColor = color5
+
+headerSubtitleColor :: Color
+headerSubtitleColor = color3
+
+footerColor :: Color
+footerColor = color4 -. 70
+
+footerHoverColor :: Color
+footerHoverColor = footerColor -. 50
+
+footerVisitedColor :: Color
+footerVisitedColor = footerColor +. 20
+
+
+--------------------------------------------------------------------------------
+fontSettings :: Css
+fontSettings = do
     star ? do
         fontFamily ["Merriweather", "Georgia"] [serif]
     code <> code ** star ? do
@@ -65,24 +136,31 @@ mainCss = do
     nav ** star ? do
         fontFamily ["Merriweather Sans"] [sansSerif]
         fontWeight bold
+
+
+--------------------------------------------------------------------------------
+mainCss :: Css
+mainCss = do
+    fontSettings
+    pageHeaderCss
+    pageFooterCss
     body ? do
         maxWidth (px 700)
         margin (px 0) auto (px 0) auto
-        backgroundColor bodyBgColor
+        backgroundColor pageBackgroundColor
         section <? do
             marginLeft (px 10)
             marginRight (px 10)
     h1 # ".section-title" ? do
-        borderBottom solid (px 1) lightBlueColor
-        color lightBlueColor
+        borderBottom solid (px 1) sectionTitleColor
+        color sectionTitleColor
         fontSize (rem 1.6)
         fontStyle italic
         marginLeft (px 20)
         marginRight (px 20)
         paddingTop (rem 1)
     article # ".post-preview" <> article # "#post" ? do
-        backgroundColor (bodyBgColor -. 10)
-        background $ vGradient (bodyBgColor -. 10) (bodyBgColor -. 5)
+        sectionBackground
         borderRadius (px 10) (px 10) (px 10) (px 10)
         margin (px 10) (px 0) (px 10) (px 0)
         padding (px 20) (px 20) (px 20) (px 20)
@@ -104,8 +182,7 @@ mainCss = do
         li <? do
             fontSize (rem 1.2)
             display inlineBlock
-            backgroundColor (bodyBgColor -. 10)
-            background $ vGradient (bodyBgColor -. 10) (bodyBgColor -. 5)
+            menuBackground
             borderRadius (em 0.5) (em 0.5) (em 0.5) (em 0.5)
             margin (rem 1) (rem 0) (rem 1) (rem 1)
             a <? do
@@ -115,20 +192,16 @@ mainCss = do
                 textDecoration none
                 padding (em 0.5) (em 0.5) (em 0.5) (em 0.5)
         li # hover <? do
-            backgroundColor menuItemBgColor
-            background $ vGradient (menuItemBgColor -. 10) menuItemBgColor
+            menuHoverBackground
             a <? do
-                color menuItemColor
-    pageHeaderCss
-    pageFooterCss
+                color menuItemHoverColor
 
 
 --------------------------------------------------------------------------------
 pageHeaderCss :: Css
 pageHeaderCss = do
     header # "#page-header" ? do
-        backgroundColor titleBgColor
-        background $ vGradient (darkBlueColor +. 30) darkBlueColor
+        headerBackground
         borderRadius (px 0) (px 0) (px 20) (px 20)
         marginTop (px 0)
         padding (px 20) (px 20) (px 0) (px 20)
@@ -140,10 +213,12 @@ pageHeaderCss = do
         margin (px 0) auto (px 0) auto
         textAlign $ alignSide sideRight
         (h1 <> (h1 ** star)) <? do
-            color titleH1Color
-            fontSize (rem 3.0)
+            color headerTitleColor
+            textStroke headerTitleBorderColor
+            fontSmooth
+            fontSize (rem 4.0)
         (h2 <> (h2 ** star)) <? do
-            color titleH2Color
+            color headerSubtitleColor
     nav # "#page-nav" ? do
         display block
         margin (px 0) auto (px 0) auto
@@ -156,8 +231,7 @@ pageHeaderCss = do
         li <? do
             fontSize (rem 1.2)
             display inlineBlock
-            backgroundColor menuItemBgColor
-            background $ vGradient (menuItemBgColor -. 10) menuItemBgColor
+            menuBackground
             a <? do
                 color menuItemColor
                 display block
@@ -165,8 +239,7 @@ pageHeaderCss = do
                 textDecoration none
                 padding (em 1) (em 1) (em 1) (em 1)
         li # hover <? do
-            backgroundColor whiteColor
-            background $ vGradient (whiteColor -. 10) whiteColor
+            menuHoverBackground
         li # firstChild <? do
             borderRadius (px 20) (px 0) (px 0) (px 0)
         li # lastChild <? do
@@ -180,5 +253,6 @@ pageFooterCss = do
         color footerColor
         textAlign $ alignSide sideCenter
         marginTop (rem 3)
-        a # link ? color blueColor
-        a # visited ? color lightBlueColor
+        a # link ? color footerColor
+        a # visited ? color footerVisitedColor
+        a # hover ? color footerHoverColor
