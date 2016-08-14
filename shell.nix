076c72aed2fa5ee4ee637b64a9a13e7bb419b194
Andreas Herrmann <andreash87@gmx.ch>
2016-08-14 15:29:25 +0200

    update shell.nix

--- shell.nix
+++ shell.nix
@@ -1,18 +1,53 @@
-with import <nixpkgs> { };
+{ system ? builtins.currentSystem
+, nixpkgs ? <nixpkgs>
+, pkgs ? (import nixpkgs { inherit system; })
+, compiler ? "default"
+, withProfiling ? false
+, withHoogle ? false
+}:
+
+with pkgs;
 
 let
-  haskell = haskellPackages.ghcWithHoogle (pkgs: with pkgs; [
-    cabal-install
-    clay
-    datetime
-    ghc-mod
-    hakyll
-    hyphenation
-  ]);
-in
+  haskellPackages =
+    let
+      hp =
+        if compiler == "default"
+          then pkgs.haskellPackages
+          else pkgs.haskell.packages.${compiler};
+      addProfiling = hp:
+        hp.override {
+          overrides = self: super: {
+            mkDerivation = args: super.mkDerivation (args // {
+              enableLibraryProfiling = true;
+            });
+          };
+        };
+      addHoogle = hp:
+        hp.override {
+          overrides = self: super: {
+            ghc = super.ghc // { withPackages = super.ghc.withHoogle; };
+            ghcWithPackages = self.ghc.withPackages;
+          };
+        };
+      applyIf = cond: f: x: if cond then f x else x;
+    in
+      applyIf withProfiling addProfiling (
+        applyIf withHoogle addHoogle (
+          hp
+        )
+      );
 
-runCommand "dummy" {
-  buildInputs = [
-    haskell
-  ];
-} ""
+  ghc =
+    haskellPackages.ghcWithPackages (pkgs: with pkgs; [
+      clay
+      datetime
+      hakyll
+      hyphenation
+    ]);
+in
+  pkgs.stdenv.mkDerivation {
+    name = "blog-env";
+    buildInputs = [ ghc ];
+    shellHook = "eval $(egrep ^export ${ghc}/bin/ghc)";
+  }
